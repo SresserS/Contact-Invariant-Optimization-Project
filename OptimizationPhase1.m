102f477a9195515478ee85b0bbd055a6a6ce5df4
SresserS <79086078+SresserS@users.noreply.github.com>
2022-12-05 11:35:20 +0800

    Update OptimizationPhase1.m

--- OptimizationPhase1.m
+++ OptimizationPhase1.m
@@ -1,7 +1,21 @@
 function [ cost ] = OptimizationPhase1( s )
-%OPTIMIZATIONPHASE1 Summary of this function goes here
-%   Detailed explanation goes here
 [cost, f, u, pose_dotdot ] = L_Physics(s);
 [cost] = L_Task(s, f, u, pose_dotdot);
 end
 
+function [ cost ] = OptimizationPhase2( s )
+lci = L_CI(s);
+[lphysics,f, u, pose_dotdot] = L_Physics(s);
+ltask = L_Task(s,f,u,pose_dotdot);
+lhint = L_Hint(s);
+lphysics = 0.000001*lphysics
+cost = lci + ltask + lphysics + lhint;
+end
+
+function [ cost ] = OptimizationPhase3(s)
+lci= L_CI(s);
+[lphysics,f, u, pose_dotdot] = L_Physics(s);
+ltask = L_Task(s,f,u,pose_dotdot);
+lphysics = 0.00001*lphysics
+cost = lci + lphysics + ltask;
+end
